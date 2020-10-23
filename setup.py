00474d62266c14a636563475393cb539a7b5c439
Idan Beck <idanbeck@gmail.com>
2020-10-23 13:26:02 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Basic setup script for the pyjunk repo/package
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='pyjunk',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT License',
+)
